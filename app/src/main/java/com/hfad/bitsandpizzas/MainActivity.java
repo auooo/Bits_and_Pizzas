7f75ed8a5acf8e676141210192930e3adc796208
auooo <mh245500@gmail.com>
2015-12-30 23:22:49 +0800

    chapter 10 final version

--- app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
@@ -1,22 +1,140 @@
 package com.hfad.bitsandpizzas;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View    ;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 import android.widget.ShareActionProvider;
 
 public class MainActivity extends Activity {
     private ShareActionProvider shareActionProvider;
+    private String[] titles;
+    private ListView drawerList;
+    private DrawerLayout drawerLayout;
+    private int currentPosition = 0;
+    private ActionBarDrawerToggle drawerToggle;
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("position", currentPosition);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred
+        drawerToggle.syncState();
+    }
+
+    // Called whenever we call invalidateOptionsMenu()
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the drawer is open, hide action items related to the content view
+        boolean drawerOpen = drawerLayout.isDrawerOpen(drawerList);
+        menu.findItem(R.id.action_share).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        titles = getResources().getStringArray(R.array.titles);
+        drawerList = (ListView) findViewById(R.id.drawer);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        // Initialize the ListView
+        // Populate the drawer's ListView and get it to respond to clicks
+        drawerList.setAdapter(new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, titles));
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
+        // Display the correct fragment
+        if (savedInstanceState != null) {
+            currentPosition = savedInstanceState.getInt("position");
+            // If the activity's been destroyed and recreated, set the correct action bar title
+            setActionBarTitle(currentPosition);
+        } else {
+            // Display TopFragment by default
+            selectItem(0);
+        }
+        // Create the ActionBarToggle
+        drawerToggle = new ActionBarDrawerToggle(this,
+                drawerLayout, R.string.open_drawer, R.string.close_drawer) {
+            // Called when a drawer has settled in a completely closed state
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                // The invalidateOptionsMenu() method tells android to recreate the menu items
+                // Call invalidateOptionsMenu when the drawer is open or closed because we want
+                // to change the action items displayed in the action bar
+                invalidateOptionsMenu();
+            }
+
+            // Called when the drawer has settled in a completely open state
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+        drawerLayout.setDrawerListener(drawerToggle);
+        // enable the icon in the action bar so we can use it to open the drawer
+        if (getActionBar() != null) {
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+            getActionBar().setHomeButtonEnabled(true);
+        } else {
+            Log.e("NullPointerException", "returned by getActionBar() method : line 105");
+        }
+
+
+        getFragmentManager().addOnBackStackChangedListener(
+                new FragmentManager.OnBackStackChangedListener() {
+            @Override
+            public void onBackStackChanged() {
+                // This gets called when the back stack changes
+                // Check which class the fragment currently attached to the activity is an instance
+                // of, and set currentPosition accordingly
+                FragmentManager fragmentManager = getFragmentManager();
+                Fragment fragment = fragmentManager.findFragmentByTag("visible_fragment");
+                if (fragment instanceof TopFragment) {
+                    currentPosition = 0;
+                }
+                if (fragment instanceof PizzaFragment) {
+                    currentPosition = 1;
+                }
+                if (fragment instanceof PastaFragment) {
+                    currentPosition = 2;
+                }
+                if (fragment instanceof StoreFragment) {
+                    currentPosition = 3;
+                }
+                // Set the action bar title and highlight the correct item in the drawer ListView
+                setActionBarTitle(currentPosition);
+                drawerList.setItemChecked(currentPosition, true);
+            }
+        });
     }
 
+    // Add items in the menu resource file to the action bar
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present
@@ -27,8 +145,13 @@ public class MainActivity extends Activity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    // This method is called when the user clicks on an item in the action bar
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // If the ActionBarDrawerToggle is clicked, let it handle what happens
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
         switch (item.getItemId()) {
             case R.id.action_create_order:
                 Intent intent = new Intent(this, OrderActivity.class);
@@ -43,10 +166,70 @@ public class MainActivity extends Activity {
         }
     }
 
+    // Pass the Share action an intent for it to share
     private void setIntent(String text) {
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("text/plain");
         intent.putExtra(Intent.EXTRA_TEXT, text);
         shareActionProvider.setShareIntent(intent);
+    }
+
+    // The OnItemClickListener's onItemClick() method gets called when the user clicks on an item
+    // in the drawer's ListView
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            // Code to run when an item in the navigation drawer gets clicked
+            // Call the selectItem() method when an item in the drawer ListView is clicked
+            selectItem(position);
+        }
+    }
+
+    // We call the selectItem() method when the user clicks on an item in the drawer's ListView
+    private void selectItem(int position) {
+        currentPosition = position;
+        Fragment fragment;
+        // Decide which fragment to display based on the position of the item the user selects in
+        // the drawer's ListView
+        switch (position) {
+            case 1:
+                fragment = new PizzaFragment();
+                break;
+            case 2:
+                fragment = new PastaFragment();
+                break;
+            case 3:
+                fragment = new StoreFragment();
+                break;
+            default:
+                fragment = new TopFragment();
+        }
+        // Display the fragment
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        // the "visible_fragment" adds a tag of "visible_fragment" to the fragment
+        // as it's added to the back stack
+        ft.replace(R.id.content_frame, fragment, "visible_fragment");
+        ft.addToBackStack(null);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.commit();
+        // set the action bar title
+        setActionBarTitle(position);
+        // Close the drawer
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(drawerList);
+    }
+
+    private void setActionBarTitle(int position) {
+        String title;
+        if (position == 0) {
+            title = getResources().getString(R.string.app_name);
+        } else {
+            title = titles[position];
+        }
+        if (getActionBar() !=null) {
+            getActionBar().setTitle(title);
+        } else {
+            Log.e("NullPointerException", "returned by getActionBar() method : line 230");
+        }
     }
 }
